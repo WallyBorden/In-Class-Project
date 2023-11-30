c43c01214100bae1180dc40f245285f8e4fef518
Zae Moore <emoore06@syr.edu>
2023-11-30 13:46:41 -0500

    F is for friends who do stuff together

--- main_script.py
+++ main_script.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import ballPos
 
 #Initial conditions/static values
 m_gas = 10**-23 #Mass of each gas particle in kg (Change later)
@@ -20,18 +21,22 @@ dt = 10**-3 #s
 i = 0
 
 #Array of gas particle momentums and positions
-px_gas = np.random.uniform(p_gas_min, p_gas_max, num_particles)
-py_gas = np.random.uniform(p_gas_min, p_gas_max, num_particles)
+px0_gas = np.random.uniform(p_gas_min, p_gas_max, num_particles)
+py0_gas = np.random.uniform(p_gas_min, p_gas_max, num_particles)
 x0_gas = np.random.uniform(0, box_width, num_particles)
 y0_gas = np.random.uniform(0, box_height, num_particles)
 
 class Ball:
-    def __init__(self, dt, x0, y0, px0, py0, r_ball, m_ball, m_gas, box_width, box_height, num_particles):
+    def __init__(self, dt, x0, y0, x0_gas, y0_gas, px0, py0, px0_gas, py0_gas, r_ball, m_ball, m_gas, box_width, box_height, num_particles):
         self.dt = dt
         self.x0 = x0
         self.y0 = y0
+        self.x0_gas = x0_gas
+        self.y0_gas = y0_gas
         self.px0 = px0
         self.py0 = py0
+        self.px0_gas = px0_gas
+        self.py0_gas = py0_gas
         self.r_ball = r_ball
         self.m_ball = m_ball
         self.m_gas = m_gas
@@ -40,17 +45,32 @@ class Ball:
         self.num_particles = num_particles
 
     def ball_move(self):
-        return 1
+        new = ballPos()
+        return new
     
     def particle_move(self):
         return 1
     
 
-x = x0_ball
-y = y0_ball
-px = px0_ball
-py = py0_ball
+x = [x0_ball]
+y = [y0_ball]
+px = [px0_ball]
+py = [py0_ball]
+x_gas = [x0_gas]
+y_gas = [y0_gas]
+px_gas = [px0_gas]
+py_gas = [py0_gas]
 while i < num_steps:
-    iteration = Ball(dt, x, y, px, py, r_ball, m_ball, m_gas, box_width, box_height, num_particles)
-
+    iteration = Ball(dt, x[-1], y[-1], x_gas[-1], y_gas[-1], px[-1], py[-1], px_gas[-1], py_gas[-1], r_ball, m_ball, m_gas, box_width, box_height, num_particles)
+    new = Ball.ball_move()
+    #Update variables
     i+=1
+
+plt.figure()
+plt.scatter(x, y, color="red")
+plt.title("Position")
+plt.xlabel("x (m)")
+plt.ylabel("y (m)")
+plt.show()
+
+plt.figure()
